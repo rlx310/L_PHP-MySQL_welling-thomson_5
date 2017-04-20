e5fcd674394d9a6958ff3e419fd071483844123b
RolandoDU <rolandolopez3442@yahoo.com>
2017-04-20 12:41:22 -0700

    end ch02

--- roLo/ch02/viewOrders.php
+++ roLo/ch02/viewOrders.php
@@ -1,6 +1,6 @@
 <?php
     $document_root = $_SERVER['DOCUMENT_ROOT'];
-    echo $document_root;
+    echo "<p>$document_root</p>";
 ?>
 <!DOCTYPE html>
 <html>
@@ -10,6 +10,24 @@
 <body>
     <h1>Bob's Auto Parts</h1>
     <h2>Customer Orders</h2>
+    <?php
 
+        @$fp = fopen("$document_root/roLo/ch02/orders.txt", 'rb');
+        flock($fp, LOCK_SH);
+
+        if (!$fp) {
+            echo "<p><strong>No orders pending.<br />Please try again later.</strong></p>";
+            exit;
+        }
+
+        while (!feof($fp)) {
+            $order = fgets($fp);
+            echo htmlspecialchars($order) . "<br />";
+        }
+
+        flock($fp, LOCK_UN);
+        fclose($fp);
+
+    ?>
 </body>
 </html>
